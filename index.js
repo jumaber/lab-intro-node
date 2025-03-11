9dc64e27b3817092c5412b39b1c686fc2151d88a
Julia Mari Bernaus <jumaber@gmail.com>
2025-03-11 22:07:25 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); // add item to array
+    this.items.sort((a,b) => a - b ); // sort array in ascending order
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if(pos < 0 || pos >= this.items.length){
+      throw new Error ("OutofBounds");
+    }
+        return this.items[pos];
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
 
-  avg() {}
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  
+  }
+
+  sum() {
+    let sum = 0;
+    this.items.forEach( num => {
+      sum += num;
+    });
+    return sum
+  }
+
+  avg() {
+
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    
+    let sum = 0;
+    this.items.forEach( num => {
+      sum += num;
+    });
+    return this.sum() / this.items.length;
+}
 }
 
 module.exports = SortedList;
